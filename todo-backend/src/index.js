3932fdf838ea278b3a01792faa591d583f4cd1ce
matiasperez <matiasperezequiel@gmail.com>
2024-09-30 13:14:26 -0300

    add frontend files and modified cors backend

--- todo-backend/src/index.js
+++ todo-backend/src/index.js
@@ -11,6 +11,14 @@ app.use(express.json());
 // Inicializar la base de datos
 initializeDatabase();
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+	next();
+});
+
 // Configurar rutas
 app.use("/api/tasks", taskRoutes);
 
---
+++ todo-frontend/src/components/Task.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "../assets/task.css";
+
+export default function Task(props) {
+  return (
+    <div>
+      <h4>{props.id} - {props.name}</h4>
+      <span>Descripcion: {props.description}</span>
+      <br />
+      <span>Fecha: {props.date}</span>
+      <br />
+      <span>Duracion: {props.duration} Minutos</span>
+      <br />
+      <span>Servicio: {props.type} </span>
+      <br />
+      {/* <button id="boton-naranja" onClick={putTask(props.id)}>Editar</button>
+      <button id="boton-rojo">Eliminar</button>
+      <button id="boton-verde">{props.boton}</button> */}
+    </div>
+  );
+}
