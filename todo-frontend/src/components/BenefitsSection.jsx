fac1820a69f93169dec4772310cd19d77ccfd1ce
melanieescobar26@gmail.com <melanie@MacBook-Air-de-Melanie.local>
2024-10-06 16:15:06 -0300

    Inicio con estilos tailwind

--- todo-frontend/src/components/BenefitsSection.jsx
+++ todo-frontend/src/components/BenefitsSection.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 
 const BenefitsSection = () => {
     return (
-        <section className="flex items-center justify-center p-8 bg-gray-100">
-            <img src="/path/to/your/image.jpg" alt="Benefits" className="w-1/2" />
-            <div className="ml-4">
-                <h2 className="text-2xl font-bold">Benefits Title</h2>
-                <p>Some text highlighting the benefits of your service.</p>
+
+        <section className="flex justify-center p-8 xl:p-32 bg-red-50 flex-col">
+            <h2 className="text-xl font-bold text-yellow-700 mb-5 md:text-4xl">Beneficios</h2>
+            <div className='flex justify-center flex-col md:flex-row'>
+            
+                <div className='flex items-center justify-center md:w-2/3'>
+                    <img src="/src/assets/img/beneficios.png" alt="Imagen que muestra beneficios de la relajación" />
+                </div>
+            <div className="mt-5 md:w-1/3 md:ml-10 flex flex-col justify-center">
+                <p className=' text-amber-600 font-extrabold md:text-3xl mb-5'>Alivia el dolor</p>
+                <p className=' text-amber-600 font-extrabold md:text-3xl mb-5'>Quema calorías</p>
+                <p className=' text-amber-600 font-extrabold md:text-3xl mb-5'>Fortalece la inmunidad</p>
+                <p className=' text-amber-600 font-extrabold md:text-3xl mb-5'>Desintoxica</p>
+                <p className=' text-amber-600 font-extrabold md:text-3xl mb-5'>Rejuvenece la piel</p>
+                <p className=' text-amber-600 font-extrabold md:text-3xl '>Mejora mental</p>
             </div>
+            </div>
+            
+            
+            
         </section>
     );
 };
--- todo-frontend/src/components/ServicesSection.jsx
+++ todo-frontend/src/components/ServicesSection.jsx
@@ -2,12 +2,37 @@ import React from 'react';
 
 const ServicesSection = () => {
     return (
-        <section className="flex items-center justify-center p-8">
-            <img src="/path/to/your/image.jpg" alt="Services" className="w-1/2" />
-            <div className="ml-4">
-                <h2 className="text-2xl font-bold">Services Title</h2>
-                <p>Some text explaining the services you offer.</p>
+        <section className=" flex justify-center p-8 xl:p-32 bg-red-50 flex-col ">
+            <h3 className="text-xl md:text-4xl font-bold text-yellow-700 mb-1">Servicios</h3>
+            <div className='flex items-center justify-center flex-col md:flex-row'>
+                <div className='m-4 bg-yellow-100 lg:size-3/12'>
+                    <div className='py-7 px-0 flex items-center justify-center flex-col'>
+                        <img src="/src/assets/img/sauna.png" alt="Imagen servicio sauna infrarrojo" className="w-1/2" />
+                        <p className='md:text-lg font-bold'>Sauna infrarrojo</p>
+                    </div>
+                </div>
+                <div className='m-4 bg-yellow-100 lg:size-3/12'>
+                    <div className='py-7 px-0 flex items-center justify-center flex-col'>
+                        <img src="/src/assets/img/aromaterapia.png" alt="Imagen servicio aromaterapia" className="w-1/2" />
+                        <p className='md:text-lg font-bold'>Aromaterapia</p>
+                    </div>
+                </div>
+                <div className='m-4 bg-yellow-100 lg:size-3/12'>
+                    <div className='py-7 px-0 flex items-center justify-center flex-col'>
+                        <img src="/src/assets/img/terapiaSal.png" alt="Imagen servicio terapia con sal" className="w-1/2" />
+                        <p className='md:text-lg font-bold'>Terapia con sal</p>
+                    </div>
+                </div>
+                <div className='m-4 bg-yellow-100 lg:size-3/12'>
+                    <div className='py-7 px-0 flex items-center justify-center flex-col'>
+                        <img src="/src/assets/img/saunaVapor.png" alt="Imagen servicio sauna vapor" className="w-1/2" />
+                        <p className='md:text-lg font-bold'>Sauna vapor</p>
+                    </div>
+                    
+                </div>
+                
             </div>
+            
         </section>
     );
 };
