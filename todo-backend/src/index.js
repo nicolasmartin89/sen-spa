4448b15e3b623f3a685faca3ce6eba6496bd1e13
Nicolas Martin <nicolasmartin89@gmail.com>
2024-10-21 17:20:57 -0300

    Add frontend route deployed.

--- todo-backend/src/index.js
+++ todo-backend/src/index.js
@@ -1,34 +1,38 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { initializeDatabase } = require("./config/db");
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Configurar CORS para permitir solicitudes desde tu frontend en Vercel
+const corsOptions = {
+  origin: "https://todo-frontend-three-dusky.vercel.app", // Tu URL del frontend en Vercel
+  methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: [
+    "Authorization",
+    "X-API-KEY",
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Access-Control-Allow-Request-Method",
+  ],
+  credentials: true, // Si es necesario compartir cookies o tokens
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Inicializar la base de datos
 initializeDatabase();
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, DELETE, PATCH"
-  );
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
-  next();
-});
 
 // Configurar rutas
 app.use("/api/tasks", taskRoutes);
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
